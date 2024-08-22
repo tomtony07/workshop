44d551b58255c443e6d3bba5aa8e597b3a813aec
tomtony7 <tomtony338@gmail.com>
2024-08-22 15:28:24 +0530

    first commit

--- app.py
+++ app.py
@@ -1,63 +1,35 @@
-from flask import Flask, render_template, request
-from fastai.vision.all import *
-from fastai.vision import *
-from fastai import *
-from PIL import Image 
-import sys
-import pathlib
-from pycaret.regression import *
-import pandas as pd
-import base64
-import io
-
-if sys.platform == "win32":
-  temp = pathlib.PosixPath
-  pathlib.PosixPath = pathlib.WindowsPath
-  del temp
-
+from flask import Flask, render_template, request, jsonify
+import numpy as np
+import sklearn
+import pickle
 
 app = Flask(__name__)
 
-vision_model = load_learner('./model/homer_bart_classifier.pkl')
-insurance_model = load_model('./model/insurance_gbr_model') 
+with open("./model/svm_model.pkl", 'rb') as file:
+    model = pickle.load(file)
 
+@app.route('/')
+def hello_world():
+    return render_template('glasstypefinder.html')
 
-# Route for the insurance form page (GET request)
-@app.route('/', methods=['GET'])
-def home():
-    return render_template('insurance.html')
+@app.route('/find-glass', methods=['POST'])
+def classify():
+      x1 = request.form.get('ri')
+      x2 = request.form.get('na')
+      x3 = request.form.get('mg')
+      x4 = request.form.get('al')
+      x5 = request.form.get('si')
+      x6 = request.form.get('k')
+      x7 = request.form.get('ca')
 
-# Route for the image classify page (GET request)
-@app.route('/image_classify', methods=['GET'])
-def bart():
-    return render_template('image.html')
+      data = [int(x1), int(x2), int(x3), int(x4), int(x5), int(x6), int(x7)]
 
-# Route for form submission (POST request)
-@app.route('/insurance_predict', methods=['POST'])
-def process_form():
-    data = {}
-    data['age'] = [request.form.get('age')]
-    data['sex'] = [request.form.get('sex')]
-    data['bmi'] = [request.form.get('bmi')]
-    data['children'] = [request.form.get('children')]
-    data['smoker'] = [request.form.get('smoker')]
-    data['region'] = [request.form.get('region')]
-    data = pd.DataFrame(data)
-    result = predict_model(insurance_model, data=data)  
-    return render_template('result.html', result=result.values[0][6])
+      X = np.array(data).reshape(1, -1)
 
-# Route for uploading an image (POST request)
-@app.route('/classify', methods=['POST'])
-def upload_image():
-    img = Image.open(request.files['input_image'].stream)
-    tensor_img = tensor(img)  # converts the image to tensor.
-    result = vision_model.predict(tensor_img)[0].capitalize()
+      result = model.predict(X)
 
-    data = io.BytesIO()
-    img.save(data, "JPEG")
-    encoded_img_data = base64.b64encode(data.getvalue())
-
-    return render_template('result.html', result=result, img_data = encoded_img_data.decode('utf-8'))
+      glass_type = result[0].item()
+      return jsonify({'glassType': glass_type})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+       app.run()
